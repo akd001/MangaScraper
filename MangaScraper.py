1a9d84286f75dfa8201f61192aa95affd090ca38
ambar.de <ambar.de@creditvidya.com>
2017-06-15 12:49:06 +0530

    Changes to methods to support downloading from websites that block urllib

--- MangaScraper.py
+++ MangaScraper.py
@@ -1,48 +1,71 @@
 import os.path
 import urllib
-import urllib2
 import urlparse
+import requests
 import ConfigParser
 from bs4 import BeautifulSoup
 
-mangaName = "naruto"			# enter the part in []: http://www.mangapanda.com/[naruto]/618/2
-saveMangaFolder = "/home/username/Desktop/MangaCrawler/Naruto"	#Save path on your system
-mangaChapter = 700						#Last chapter to be crawled
+mangaName = "one-piece"			# enter the part in []: http://www.mangapanda.com/[naruto]/618/2
+saveMangaFolder = "/home/akd/Desktop/Workspace/MangaScraper/" + mangaName	#Save path on your system
+mangaChapterMax = 2						#Last chapter to be crawled
 
-def imgLink (mangaChapter, mangaSheet):
+mangaPage = ""
+mangaChapter = 0
+mangaSheet = 0
+imageUrl = ""
+
+def getMangaPageUrl ():
+	global mangaPage
 	mangaPage = "http://www.mangapanda.com/" + mangaName + "/" + str(mangaChapter) + "/" + str(mangaSheet)
-	htmlPage = BeautifulSoup (urllib.urlopen(mangaPage).read())
-	if (htmlPage.body.h1.string == "404 Not Found"):
+	# print "Manga Page: " + mangaPage
+	return mangaPage
+
+def getHtmlPageForUrl ():
+	return BeautifulSoup (urllib.urlopen(getMangaPageUrl()).read(), "html.parser")
+
+def getMangaChapterName ():
+
+
+def getImageUrlIfAvailable ():
+	global imageUrl
+	htmlPage = getHtmlPageForUrl()
+	try:
+		divImage = htmlPage.body.table.img['src']
+		imageUrl = urlparse.urljoin(mangaPage, divImage)
+		return True
+	except AttributeError:
 		print "Next Chapter!"
-		return None
-	divImage1 = htmlPage.body.table.img['src']
-	return urlparse.urljoin(mangaPage, divImage1)
+		return False
 
-def imgSave (imageLink, mangaChapter, mangaSheet):
-	imageData = urllib2.urlopen(imageLink).read()
-	savePath = os.path.join(saveMangaFolder, str (mangaChapter) + "-" + str(mangaSheet) + ".jpg")
-	output = open(savePath, 'wb')
-	output.write(imageData)
-	output.close()
+def getSavePath ():
+	return os.path.join(saveMangaFolder, str(mangaChapter) + "-" + str(mangaSheet) + ".jpg")
 
-# To download all the chapters, mention the last chapter number of the manga [mangaChapter (integer)]
-for j in range(1, mangaChapter):
+def saveImageIntoJpeg ():		#some websites block urllib downloads, use saveFile instead
+	print "Image Link: " + imageUrl
+	savePath = getSavePath()
+	print "Being saved into: " + savePath
+	urllib.urlretrieve(imageUrl, savePath)
+
+def saveFile (url):
+	print "URL to be saved: " + url
+	local_filename = getSavePath()
+	r = requests.get(url)
+	f = open(local_filename, 'wb')
+	for chunk in r.iter_content(chunk_size=512 * 1024): 
+		if chunk: # filter out keep-alive new chunks
+			f.write(chunk)
+	f.close()
+	return 
+
+# To download all the chapters, mention the last chapter number of the manga [mangaChapterMax (integer)]
+for j in range(1, mangaChapterMax):
 	for i in range(1, 100):
-		imageLink = imgLink(j, i)
-		if imageLink != None:
-			print imageLink
-			imgSave(imageLink, j, i)
+		mangaChapter = j
+		mangaSheet = i
+		if getImageUrlIfAvailable():
+			saveFile(imageUrl)
+			# saveImageIntoJpeg()
 		else:
 			break
-
-
-#To download a single chapter, mention the mangaChapter (integer) in the beginning
-# for i in range(1, 100):
-# 		imageLink = imgLink(mangaChapter, i)
-# 		if imageLink != None:
-# 			print imageLink
-# 			imgSave(imageLink, mangaChapter, i)
-# 		else:
-# 			break
 
 print "All done! :)"
